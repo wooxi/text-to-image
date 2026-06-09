8be1fced2ac06f56dc90dcedd6770fb167390a04
root <root@Ubuntu.example.invali>
2026-06-09 22:24:27 +0800

    fix(android): enable file access, DOM storage, download listener
    - AndroidManifest: added WRITE/READ_EXTERNAL_STORAGE permissions
    - MainActivity: configured WebView settings for file access,
      DOM storage, database, content access, and download listener
      to trigger system browser/viewer for downloaded files

--- android/app/src/main/java/com/texttoimage/app/MainActivity.java
+++ android/app/src/main/java/com/texttoimage/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.texttoimage.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Bridge;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bridge bridge = this.bridge;
+        if (bridge != null) {
+            WebView webView = bridge.getWebView();
+            if (webView != null) {
+                webView.getSettings().setAllowFileAccess(true);
+                webView.getSettings().setDomStorageEnabled(true);
+                webView.getSettings().setDatabaseEnabled(true);
+                webView.getSettings().setAllowContentAccess(true);
+                webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+                webView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
+                    android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
+                    intent.setData(android.net.Uri.parse(url));
+                    startActivity(intent);
+                });
+            }
+        }
+    }
+}
